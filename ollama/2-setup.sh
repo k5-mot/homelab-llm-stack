597e8e9b5627204cea8109586afa62554965d1fa
k5-mot <34744243+k5-mot@users.noreply.github.com>
2024-10-20 17:54:11 +0900

    :recycle: Refactoring

--- ollama/2-setup.sh
+++ ollama/2-setup.sh
@@ -7,10 +7,6 @@
 cd /workspace
 model_dir="/workspace/model"
 
-# Clear a part of proxy
-export http_proxy=
-export HTTP_PROXY=
-
 # Run the server temporarily
 ollama serve &
 
@@ -21,6 +17,10 @@ cd ${model_dir}/llm-jp
 curl -LO https://huggingface.co/mmnga/llm-jp-13b-instruct-full-ac_001_16x-dolly-ichikara_004_001_single-oasst-oasst2-v2.0-gguf/resolve/main/llm-jp-13b-instruct-full-ac_001_16x-dolly-ichikara_004_001_single-oasst-oasst2-v2.0-Q4_K_M.gguf
 cd ${model_dir}/watashiha
 curl -LO https://huggingface.co/mmnga/watashiha-gpt-6b-gguf/resolve/main/watashiha-gpt-6b-q4_K_M.gguf
+
+# Clear a part of proxy
+export http_proxy=
+export HTTP_PROXY=
 
 # Create LLM models
 ollama create elyza3:8b    -f ${model_dir}/elyza3/Modelfile
